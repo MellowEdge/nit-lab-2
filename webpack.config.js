1fc54d05aed74d2cd009bb7fb1a26c87719add28
Simon Boytcov <bsimon20@gmail.com>
2018-11-20 14:44:25 +0200

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,19 @@ module.exports = {
         path: path.resolve(__dirname, 'docs'),
         filename: 'js/main.js',
     },
+	module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            // ...additional rules...
+        ],
+    },
 };
